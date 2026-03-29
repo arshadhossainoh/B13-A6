068df048bb93c3db7dbe0a70dcdace2284531051
arshadhossainoh <arshadhossain.oh@gmail.com>
2026-03-29 13:29:51 -0400

    my first commit & worked on navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import CartImg from "../assets/products/shopping-cart.png";
+
+const Navbar = () => {
+  return (
+    <div className="container mx-auto flex justify-between mt-5">
+      <h3 className="text-2xl font-bold text-[#4F39f6]">DigiTools</h3>
+
+      <ul className="flex gap-5">
+        <li>
+          <a href="">Products</a>
+        </li>
+        <li>
+          <a href="">Features</a>
+        </li>
+        <li>
+          <a href="">Pricing</a>
+        </li>
+        <li>
+          <a href="">Testimonials</a>
+        </li>
+        <li>
+          <a href="">FAQ</a>
+        </li>
+      </ul>
+      <div className="flex gap-5 items-center">
+        <img className="h-5 w-5" src={CartImg} alt="" />
+        <button>Login</button>
+        <button className="bg-[#4f39f6] text-white rounded-full p-1">
+          Get Started
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
